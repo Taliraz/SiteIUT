0f5b1fc01ef4d5187d4636327740e7f022972029
Taliraz <s.armangau@laposte.net>
2018-10-20 18:49:41 +0200

    Update view/view.php

---
+++ view/view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title><?php echo $pagetitle; ?></title>
+    </head>
+
+    <body>
+
+    <?php
+    $filepath = File::build_path(array("view", $controller, "$view.php"));
+    require $filepath;
+    ?>
+    </body>
+
+
+</html>
