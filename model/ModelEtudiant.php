6491cb028d258423cb301d98d8524a54f2e1646b
Cao <cao34970@gmail.com>
2018-10-20 18:04:22 +0200

    Je m'étais fail ;)

--- model/ModelEtudiant.php
+++ model/ModelEtudiant.php
@@ -1,89 +1,74 @@
-<?php 
-require "Model.php";
+<?php
+require "ModelUtilisateur.php";
 
-class ModelUtilisateur {
+class ModelEtudiant extends ModelUtilisateur {
+    private $idEtudiant;
+    private $idIUT;
+    private $nom;
+    private $prenom;
+    private $anneeInscription;
     
-    private $id;
-    private $login;
-    private $mdp;
-    
-    public function __construct($login = NULL, $mdp = NULL){
-        if(!is_null($login) && !is_null($mdp)){
-            $this->login = $login;
-            $this->mdp = $mdp; 
+    public function __construct($nom = null, $prenom = null, $email = null, $anneeInscription = null, $idIUT = null){
+        if (!is_null($nom) && !is_null($prenom) && !is_null($email) && !is_null($anneeInscription) && !is_null($idIUT)) {
+            $this->nom = $nom;
+            $this->prenom = $prenom;
+            $this->email = $email;
+            $this->anneeInscription = $anneeInscription;
+            $this->idIUT = $idIUT;
         }
     }
-    
-    public static function getId() {
-        return $this->id;
+
+    public static function getNom() {
+        return $this->nom;
     }
     
-    public static function getLogin() {
-        return $this->login;
+    public static function getPrenom() {
+        return $this->prenom;
     }
     
-    public static function getMdp() {
-        return $this->mdp;
+    public static function getEmail() {
+        return $this->email;
+    }
+    
+    public static function getAnneeInscription() {
+        return $this->anneeInscription;
+    }
+    
+    public static function getIdIUT() {
+        return $this->idIUT;
+    }
+    
+    public static function setNom($nom) {
+        $this->nom = $nom;
+    }
+    
+    public static function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+    
+    public static function setEmail($email) {
+        $this->email = $email;
+    }
+    
+    public static function setAnneeInscription($anneeInscription) {
+        $this->anneeInscription = $anneeInscription;
+    }
+    
+    public static function setIdIUT($idIUT) {
+        $this->idIUT = $idIUT;
     }
     
     
-    public static function setLogin($login){
-        $this->login = $login;
-    }
-    
-    public static function setMdp($mdp){
-        $this->login = $mdp;
-    }
-      
-    
-    public static function connexion($login, $mdp){
-        $data = array(':login'=>$login, ':mdp'=>$mdp);
-        $req = Model::$pdo->prepare("SELECT * FROM utilisateurs WHERE login = :login AND mdp = :mdp ");
-        $req->execute($data);
-        if ($check = $req->rowcount() != 1) {
-            return "Erreur - Nombre d'utilisateur diférent de 1";
-        }
-        else {
-            $req->setFetchMode(PDO::FETCH_CLASS, 'ModelVoiture');
-            $row = $req->fetch();
-            return $row;
-        }
-    }
-    
-    public static function getUtilisateurAvecId($id){
-        $req = Model::$pdo->prepare('SELECT * FROM utilisateurs WHERE id = :id');
-        $req->execute(array(':id'=>$id));
-        $check = $req->rowcount();
-        if($check == 1){
-            $req->setFetchMode(PDO::FETCH_CLASS, 'ModelUtilisateur');
-            $row = $req->fetch();
-            return $row;
-        }
-        else { return "Erreur - Utilisateur non trouvé"; }
-    }
-    
-    public static function afficherTousUtilisateurs(){
-        $req = Model::$pdo->query ("SELECT * FROM utilisateurs");
-        $req->setFetchMode(PDO::FETCH_CLASS, 'ModelUtilisateur');
-        $row = $req->fetchAll();
-        return $row; 
-    }
-    
-    public static function ajoutUtilisateur(){
-        $erreur = "utilisateur déjà présent dans la base de données";
-        $login = htmlspecialchars($this->login);
-        $mdp = sha1($this->mdp);
-        $data = array($login, $mdp);
-        $reqVerif = Model::$pdo->prepare("SELECT id FROM utilisateurs WHERE login = :login");
-        $reqVerif->execute(array(':login'=>$login));
-        $resVerif = $reqVerif->rowcount();
-        if($resVerif > 0){
-            return $erreur;
+    public static function associerUtilisateur($id){
+        $check = Model::$pdo->prepare("SELECT * FROM utilisateurs WHERE id = :id");
+        $check->execute(array(':id'=>$id));
+        $checkCount = $check->rowcount();
+        if($checkcount == 1){
+            $this->idEtudiant = $id;
+            return true;
         }
         else {
-            $insert = Model::$pdo->prepare("INSERT INTO utilisateurs(login, mdp, email) VALUES(?,?,?)");
-            $insert->execute($data); 
+            return false;
         }
-    }   
+    }
 }
-
