f6ffb814a2be474903f8d7cdfd77361fb54e84f3
Taliraz <s.armangau@laposte.net>
2018-10-20 18:58:14 +0200

    Update view/ville/create.php

---
+++ view/ville/create.php
@@ -0,0 +1,24 @@
+<html> 
+    <body>
+      <form method="post" action="index.php?action=created">
+        <fieldset>
+          <legend>Ville :</legend>
+          <p>
+            <label for="nom_id">Nom</label> :
+            <input type="text" placeholder="Ex : Montpellier" name="nom" id="nom_id" required/>
+          </p>
+          <p>
+            <label for="codePostal_id">Code Postal</label> :
+            <input type="text" placeholder="Ex : 34000" name="codePostal" id="codePostal_id" required/>
+          </p>
+          <p>
+            <label for="departement_id">Marque</label> :
+            <input type="text" placeholder="Ex : Herault" name="departement" id="departement_id" required/>
+          </p>
+          <p>
+            <input type="submit" value="Envoyer" />
+          </p>
+        </fieldset> 
+      </form>
+    </body>
+</html>
