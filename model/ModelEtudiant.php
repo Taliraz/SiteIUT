0b7d3425f99449beba9d41c4a5c03632ceaeacb9
Cao <cao34970@gmail.com>
2018-10-20 18:03:51 +0200

    Etudiant, premier lancé

---
+++ model/ModelEtudiant.php
@@ -0,0 +1,89 @@
+<?php 
+require "Model.php";
+
+class ModelUtilisateur {
+    
+    private $id;
+    private $login;
+    private $mdp;
+    
+    public function __construct($login = NULL, $mdp = NULL){
+        if(!is_null($login) && !is_null($mdp)){
+            $this->login = $login;
+            $this->mdp = $mdp; 
+        }
+    }
+    
+    public static function getId() {
+        return $this->id;
+    }
+    
+    public static function getLogin() {
+        return $this->login;
+    }
+    
+    public static function getMdp() {
+        return $this->mdp;
+    }
+    
+    
+    public static function setLogin($login){
+        $this->login = $login;
+    }
+    
+    public static function setMdp($mdp){
+        $this->login = $mdp;
+    }
+      
+    
+    public static function connexion($login, $mdp){
+        $data = array(':login'=>$login, ':mdp'=>$mdp);
+        $req = Model::$pdo->prepare("SELECT * FROM utilisateurs WHERE login = :login AND mdp = :mdp ");
+        $req->execute($data);
+        if ($check = $req->rowcount() != 1) {
+            return "Erreur - Nombre d'utilisateur diférent de 1";
+        }
+        else {
+            $req->setFetchMode(PDO::FETCH_CLASS, 'ModelVoiture');
+            $row = $req->fetch();
+            return $row;
+        }
+    }
+    
+    public static function getUtilisateurAvecId($id){
+        $req = Model::$pdo->prepare('SELECT * FROM utilisateurs WHERE id = :id');
+        $req->execute(array(':id'=>$id));
+        $check = $req->rowcount();
+        if($check == 1){
+            $req->setFetchMode(PDO::FETCH_CLASS, 'ModelUtilisateur');
+            $row = $req->fetch();
+            return $row;
+        }
+        else { return "Erreur - Utilisateur non trouvé"; }
+    }
+    
+    public static function afficherTousUtilisateurs(){
+        $req = Model::$pdo->query ("SELECT * FROM utilisateurs");
+        $req->setFetchMode(PDO::FETCH_CLASS, 'ModelUtilisateur');
+        $row = $req->fetchAll();
+        return $row; 
+    }
+    
+    public static function ajoutUtilisateur(){
+        $erreur = "utilisateur déjà présent dans la base de données";
+        $login = htmlspecialchars($this->login);
+        $mdp = sha1($this->mdp);
+        $data = array($login, $mdp);
+        $reqVerif = Model::$pdo->prepare("SELECT id FROM utilisateurs WHERE login = :login");
+        $reqVerif->execute(array(':login'=>$login));
+        $resVerif = $reqVerif->rowcount();
+        if($resVerif > 0){
+            return $erreur;
+        }
+        else {
+            $insert = Model::$pdo->prepare("INSERT INTO utilisateurs(login, mdp, email) VALUES(?,?,?)");
+            $insert->execute($data); 
+        }
+    }   
+}
+
