44aaf28a466ef8c941db74919850b502d52c3706
Taliraz <s.armangau@laposte.net>
2018-10-20 23:45:33 +0200

    Update view/stage/error.php

---
+++ view/stage/error.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <p>
+            Erreur l'id n'existe pas <a title="retour" href="index.php.php?action=readAll">Retour à l'accueil</a>
+        </p>
+    </body>
+</html>
