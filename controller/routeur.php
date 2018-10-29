f802f784647e1f7e2eb5eb732dd120639025ab15
Taliraz <s.armangau@laposte.net>
2018-10-29 20:17:40 +0100

    Update controller/routeur.php

--- controller/routeur.php
+++ controller/routeur.php
@@ -5,5 +5,57 @@ if(isset($_GET['action'])) {
 }
 else { $action="readAll"; }
 
-ControllerEtudiant::$action(); 
+$controller=$_GET['controller'];
+
+if ($controller=='etudiant')
+	ControllerEtudiant::$action(); 
+}
+
+if ($controller=='administrateur')
+	ControllerAdministrateur::$action(); 
+}
+
+if ($controller=='article')
+	ControllerArticle::$action(); 
+}
+
+if ($controller=='categorie')
+	ControllerCategorie::$action(); 
+}
+
+if ($controller=='chefDep')
+	ControllerChefDep::$action(); 
+}
+
+if ($controller=='contactEntreprise')
+	ControllerContactEntreprise::$action(); 
+}
+
+if ($controller=='entreprise')
+	ControllerEntreprise::$action(); 
+}
+
+if ($controller=='IUT')
+	ControllerIUT::$action(); 
+}
+
+if ($controller=='page')
+	ControllerPage::$action(); 
+}
+
+if ($controller=='stage')
+	ControllerStage::$action(); 
+}
+
+if ($controller=='temoignage')
+	ControllerTemoignage::$action(); 
+}
+
+if ($controller=='utilisateur')
+	ControllerUtilisateur::$action(); 
+}
+
+if ($controller=='ville')
+	ControllerVille::$action(); 
+}
 ?>
