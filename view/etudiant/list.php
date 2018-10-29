bae5d034aafc450773783c55a545d9919eb2bed4
Cao <cao34970@gmail.com>
2018-10-29 23:19:10 +0100

    ;)
    Co-Authored-By: Titouan <titouancao@users.noreply.github.com>

--- view/etudiant/list.php
+++ view/etudiant/list.php
@@ -1,4 +1,17 @@
-<?php 
-require(File::build_path(array("controller","ControllerEtudiant.php")));
-
-ControllerEtudiant::afficherTousEtudiants(); 
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo File::build_path_css(array("view","CSS" ,"List.css")); ?>">
+    </head>
+    <body>
+        <div id="espace-admin">
+            <h2 class="titre">Liste des Etudiants</h2>
+            <ul id="liste">
+            <?php 
+            foreach ($row as $valeur) {
+                echo '<p> <a href="index.php?action=details&idEtudiant='.rawurlencode($valeur->getIdUtilisateur()).'"><li class="elements-liste"> ' . htmlspecialchars($valeur->getLogin()) . '</li></a></p>';
+            }?>
+            </ul>
+            <a class="bouton" href="index.php?action=create"> Ajouter un Etudiant </a>
+        </div>
+    </body>
+</html>
