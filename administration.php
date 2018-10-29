086cd03abdfd65630f95eb6987cb59f38f7518bf
Taliraz <s.armangau@laposte.net>
2018-10-29 17:29:05 +0100

    debut panneau d'administration

---
+++ administration.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Panneau d'administration </title>
+        <link rel="stylesheet" href="CSS/administration.css" />
+    </head>
+    <body>
+      <div class="boutons">
+        <div class="bouton">
+          Administrateurs
+        </div>
+        <div class="bouton">
+          Etudiants
+        </div>
+        <div class="bouton">
+          Chef de Departement
+        </div>
+        <div class="bouton">
+          Entreprises
+        </div>
+        <div class="bouton">
+          IUT
+        </div>
+        <div class="bouton">
+          Pages
+        </div>
+        <div class="bouton">
+          Stages
+        </div>
+        <div class="bouton">
+          Temoignages
+        </div>
+        <div class="bouton">
+          Villes
+        </div>
+      </div>
+
+    </body>
+</html>
