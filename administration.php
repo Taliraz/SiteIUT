cc31bebea7ea8e29f620f47cf0ecf7d36b9a7d7a
Taliraz <s.armangau@laposte.net>
2018-10-29 19:52:33 +0100

    administration

--- administration.php
+++ administration.php
@@ -5,33 +5,39 @@
         <link rel="stylesheet" href="CSS/administration.css" />
     </head>
     <body>
-      <div class="boutons">
-        <div class="bouton">
-          Administrateurs
+      <div class="boutons_colonne">
+        <div class="boutons_ligne">
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Administrateurs
+          </a>
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Etudiants
+          </a>
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Chef de Departement
+          </a>
         </div>
-        <div class="bouton">
-          Etudiants
+        <div class="boutons_ligne">
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Entreprises
+          </a>
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            IUT
+          </a>
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Pages
+          </a>
         </div>
-        <div class="bouton">
-          Chef de Departement
-        </div>
-        <div class="bouton">
-          Entreprises
-        </div>
-        <div class="bouton">
-          IUT
-        </div>
-        <div class="bouton">
-          Pages
-        </div>
-        <div class="bouton">
-          Stages
-        </div>
-        <div class="bouton">
-          Temoignages
-        </div>
-        <div class="bouton">
-          Villes
+          <div class="boutons_ligne">
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Stages
+          </a>
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Temoignages
+          </a>
+          <a href="https://blizzcon.com/fr-fr/watch?v=5bc3ff016d411d7aa6a8ec86&eventYear=2018" class="bouton" >
+            Villes
+          </a>
         </div>
       </div>
 
