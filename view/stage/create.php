96964478b22549d0802848d188b792f849858432
Taliraz <s.armangau@laposte.net>
2018-10-30 14:30:06 +0100

    Update view/stage/create.php

--- view/stage/create.php
+++ view/stage/create.php
@@ -1,30 +1,48 @@
 <html> 
     <body>
-      <form method="post" action="index.php?action=created">
+      <form method="post" action="index.php?action=created&controller=stage">
         <fieldset>
           <legend>Stage :</legend>
           <p>
-            <label for="intitule_id">Intitule</label> :
-            <input type="text" placeholder="Ex : Developpement logiciel" name="intitule" id="intitule_id" required/>
+            <label for="intituleStage_id">Intitule</label> :
+            <input type="text" placeholder="Ex : Developpement logiciel" name="intituleStage" id="intituleStage_id" required/>
           </p>
 
           <p>
-            <label for="dateDeb_id">Date de Début</label> :
-            <input type="date" placeholder="Ex : 20/10/2018" name="dateDeb" id="dateDeb_id" required/>
+            <label for="dateDebStage_id">Date de Début</label> :
+            <input type="date" placeholder="Ex : 20/10/2018" name="dateDebStage" id="dateDebStage_id" required/>
           </p>
 
           <p>
-            <label for="dateDeb_id">Date de Fin</label> :
-            <input type="date" placeholder="Ex : 20/10/2018" name="dateFin" id="dateFin_id" required/>
+            <label for="dateFinStage_id">Date de Fin</label> :
+            <input type="date" placeholder="Ex : 20/10/2018" name="dateFinStage" id="dateFinStage_id" required/>
           </p>
 
           <p>
-            <label for="remunere_id">Stage Remunere</label> :
+            <label for="gratifie_id">Stage Gratifie</label> :
             <select name="remunere" id="remunere_id" required>
               <option value="true">Oui </option>
               <option value="false">Non </option>
             </select>
           </p>
+
+          <p>
+             <label for="descriptionStage_id">Description du Stage</label><br />
+             <textarea name="descriptionStage" id="descriptionStage_id"></textarea>
+         </p>
+
+         <p>
+            <label for="IUT_id">IUT</label> 
+            <select name="idIUT" size="1" id="IUT_id">
+              <?php 
+                $liste = ModelStage::getAllStages();
+                foreach ($liste as $valeur){
+                  echo '<option value="'.$valeur->getIdStage().'">'.$valeur->getIntituleStage().'</option>';
+                } 
+              ?>
+          </select>
+        </p>
+
 
           <p>
             <input type="submit" value="Envoyer" />
