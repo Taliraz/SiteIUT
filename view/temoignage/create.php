b0304e101b8a7f38ba6cd7cebde1972ad6543b1f
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2018-11-15 16:37:37 +0100

    Update view/temoignage/create.php

--- view/temoignage/create.php
+++ view/temoignage/create.php
@@ -4,9 +4,13 @@
         <fieldset>
           <legend>Temoignages :</legend>
           <p>
-            <label for="contenu_id">contenu</label> :
+            <label for="titreTemoignage_id"> Titre </label>
+            <input type="text" id="titreTemoignage_id" name="titreTemoignage">
+          </p>
+          <p>
+            <label for="contenuTemoignage_id">contenu</label> :
             <br>
-            <textarea name="contenu" id="contenu" required></textarea>
+            <textarea name="contenuTemoignage" id="contenuTemoignage" required></textarea>
           </p>
 
           <p>
@@ -23,7 +27,33 @@
           </p>
 
           <p>
+            <label for="nomEtudiant_id"> Nom </label>
+            <input type="text" id="nomEtudiant_id" name="nomEtudiant">
+          </p>
+
+          <p>
+            <label for="prenomEtudiant_id"> Titre </label>
+            <input type="text" id="prenomEtudiant_id" name="prenomEtudiant">
+          </p>
+
+          <p>
+              <label for="IUT_id">IUT</label> 
+              <select name="idIUT" size="1" id="IUT_id">
+                <?php
+                /*$liste=ModelIUT::getAll();
+                foreach($liste as $valeur){
+                  echo '<option value="'.$valeur->getIdIUT().'">'.$valeur->getNomIUT().'</option>';
+                }*/
+                ?>
+              </select>
+          </p>
+
+          <p>
             <input type="submit" value="Envoyer" />
+          </p>
+
+          <p>
+              <input id="bouton-retour" type="button" value="Retour" onclick="history.go(-1)">
           </p>
 
         </fieldset> 
