5fbf5e1d891b99469d5815f24cb7e23756326ffc
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2018-11-15 14:12:17 +0100

    Modif fichiers

--- view/contactEntreprise/create.php
+++
@@ -1,4 +0,0 @@
-<?php 
-require(File::build_path(array("controller","ControllerContactEntreprise.php")));
-
-ControllerContactEntreprise::ajoutContactEntreprise('contactEntreprise2', 'mdp', 'Armangau', 'Sulivend', 'machineàcafé', "0348546598", "tropnul@gmail.com", 25);
--- view/etudiant/suppression.php
+++
@@ -1,3 +0,0 @@
-<?php
-require(File::build_path(array("controller","ControllerEtudiant.php")));
-ControllerEtudiant::supprimerEtudiant(17);
