8733a59f0389a086ebb4510bb17f420cafd4ee24
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2018-11-24 11:17:55 +0100

    Update view.php

--- view/view.php
+++ view/view.php
@@ -5,6 +5,17 @@
         <title><?php echo $pagetitle; ?></title>
     </head>
 
+     <header>
+        <p style="border: 1px solid black;text-align:center;padding-right:1em;">
+            <a href="index.php?action=readAll&controller=administrateur"> Administrateur </a>
+            <a href="index.php?action=readAll&controller=article"> Articles </a>
+            <a href="index.php?action=readAll&controller=IUT">IUT </a>
+            <a href="index.php?action=readAll&controller=stage">Stage </a>
+            <a href="index.php?action=readAll&controller=temoignage">Temoignage </a>
+            <a href="index.php?action=readAll&controller=ville">Ville </a>
+        </p>
+    </header>
+
     <body>
 
     <?php
