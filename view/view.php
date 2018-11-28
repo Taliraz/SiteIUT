42d54ce146ef9fb2b14ba772d6dcd3afd1c83829
Haldrius <Jules.nofal@gmail.com>
2018-11-28 11:20:48 +0100

    Menu plus stylé

--- view/view.php
+++ view/view.php
@@ -2,19 +2,21 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
         <title><?php echo $pagetitle; ?></title>
     </head>
 
      <header>
-        <p style="border: 1px solid black;text-align:center;padding-right:1em;">
-            <a href="index.php?action=readAll&controller=administrateur"> Administrateur </a>
-            <a href="index.php?action=readAll&controller=article"> Articles </a>
-            <a href="index.php?action=readAll&controller=IUT">IUT </a>
-            <a href="index.php?action=readAll&controller=stage">Stage </a>
-            <a href="index.php?action=readAll&controller=temoignage">Temoignage </a>
-            <a href="index.php?action=readAll&controller=ville">Ville </a>
-            <a href="index.php?action=connect&controller=administrateur">Connexion </a>
+        <p>
+            <a class='buttons' href="index.php?action=readAll&controller=administrateur"> Administrateur </a>
+            <a class='buttons' href="index.php?action=readAll&controller=article"> Articles </a>
+            <a class='buttons' href="index.php?action=readAll&controller=IUT">IUT </a>
+            <a class='buttons' href="index.php?action=readAll&controller=stage">Stage </a>
+            <a class='buttons' href="index.php?action=readAll&controller=temoignage">Temoignage </a>
+            <a class='buttons' href="index.php?action=readAll&controller=ville">Ville </a>
+            <a class='buttons' href="index.php?action=connect&controller=administrateur">Connexion </a>
         </p>
+        <br>
     </header>
 
     <body>
