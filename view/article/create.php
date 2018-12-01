c3a7e59438957ecfd33ed0ec9a759d38c8ca4154
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2018-12-01 13:38:46 +0100

    Update create.php

---
+++ view/article/create.php
@@ -0,0 +1,20 @@
+<html> 
+    <body>
+      <form method="post" action="index.php?action=created">
+        <fieldset>
+          <legend>Ville :</legend>
+          <p>
+            <label for="nom_id">Nom</label> :
+            <input type="text" placeholder="Ex : Description" name="nom" id="nom_id" required/>
+          </p>
+          <p>
+             <label for="contenu_id">Contenu</label><br />
+             <textarea name="contenu" id="contenu_id"></textarea>
+          </p>
+          <p>
+            <input type="submit" value="Envoyer" />
+          </p>
+        </fieldset> 
+      </form>
+    </body>
+</html>
