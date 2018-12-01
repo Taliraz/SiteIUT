0408448873786c36666369aad6237c41bfd84d57
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2018-12-01 12:52:21 +0100

    Update ControllerAdministrateur.php

--- controller/ControllerAdministrateur.php
+++ controller/ControllerAdministrateur.php
@@ -1,18 +1,129 @@
-<?php 
-require(File::build_path(array("model","ModelUtilisateur.php")));
+<?php
+require_once (File::build_path(array("model","ModelAdministrateur.php"))); // chargement du modèle
+require_once (File::build_path(array("lib","Security.php")));
+class ControllerAdministrateur {
 
-class ControllerUtilisateur {
-    public static function connexion($login, $mdp){
-        $connexion = ModelUtilisateur::connexion($login, $mdp);
-        if (gettype($connexion) == 'string') {
-            echo $connexion;
+
+    public static function readAll() {
+        $tab_v = ModelAdministrateur::getallAdministrateurs();     //appel au modèle pour gerer la BD
+        $controller='administrateur';
+        $view='list';
+        $pagetitle='liste des Administrateurs';
+        require (File::build_path(array("view","view.php")));  //"redirige" vers la vue
+    }
+
+     public static function read(){
+        $v=ModelAdministrateur::getAdministrateurByLogin($_GET ['login']);
+        if ($v==false){
+            $controller='administrateur';
+            $view='erreur';
+            $pagetitle='Erreur';
+            require (File::build_path(array("view","view.php")));
         }
-        else {
-            session_start();
-            $_SESSION['id'] = $connexion['id'];
-            $_SESSION['login'] = $connexion['login'];
+        else{
+            $controller='administrateur';
+            $view='detail';
+            $pagetitle='Detail';
+            require(File::build_path(array("view","view.php")));
+        }
+        
+    }
+
+    public static function delete(){
+        $login=$_GET['login'];
+        if (isset($_SESSION['login'])){
+            $admin=ModelAdministrateur::getAdministrateurByLogin($login);
+            $admin->delete();
+            $tab_v=ModelAdministrateur::getallAdministrateurs();
+            $controller='administrateur';
+            $view='deleted';
+            $pagetitle='administrateur supprimé';
+            require(File::build_path(array("view","view.php")));
+        }
+        else{
+            self::readAll();
         }
     }
+
+
+    public static function create(){
+        $controller='administrateur';
+        $view='create';
+        $pagetitle='Creation de administrateur';
+        require(File::build_path(array("view","view.php")));
+    }
+
+    public static function connect(){
+        $controller='administrateur';
+        $view='connect';
+        $pagetitle='Connexion';
+        require(File::build_path(array("view","view.php")));
+    }
+
+    public static function disconnect(){
+        session_unset();
+        session_destroy();
+        setcookie(session_name(),'',time()-1);
+        self::readAll();
+    }
+
+
+    public static function created(){
+      $ModelAdministrateur=new ModelAdministrateur($_POST['login'],Security::chiffrer($_POST['mdp']));
+      $ModelAdministrateur->save();
+      $controller='administrateur';
+      $view='created';
+      $pagetitle='Administrateur créé';
+      require(File::build_path(array("view","view.php")));
+    }
+    
+    public static function update(){
+        $login=$_GET ['login'];
+        if (isset($_SESSION['login'])){
+            $v=ModelAdministrateur::getadministrateurbylogin($login);
+            $controller='administrateur';
+            $view='update';
+            $pagetitle='modification de administrateur';
+            require(File::build_path(array("view","view.php")));
+        }
+        else{
+            self::readAll();
+        }
+    }
+
+    public static function updated(){
+        $login=$_GET['login'];
+        if (isset($_SESSION['login'])){
+            $ModelAdministrateur=new ModelAdministrateur($_POST['login'],$_POST['nom'],$_POST['prenom'],Security::chiffrer($_POST['mdp']),false);
+            $ModelAdministrateur->update(ModelAdministrateur::getadministrateurbylogin($login));
+            $controller='administrateur';
+            $view='updated';
+            $pagetitle='Administrateur modifié';
+            require(File::build_path(array("view","view.php")));
+        }
+        else{
+            self::readAll();
+        }
+
+    }
+
+    public static function connected(){
+        $compte=ModelAdministrateur::checkPassword($_POST['login'],Security::chiffrer($_POST['mdp']));
+        if ($compte!=false){
+            $_SESSION['login']=$_POST['login'];
+            $v=ModelAdministrateur::getadministrateurbylogin($_POST['login']);
+            $controller='administrateur';
+            $view='detail';
+            $pagetitle='Connecté';
+            require(File::build_path(array("view","view.php")));
+        }
+        else{
+            echo '<p>Mot de passe erroné</p>';
+            $controller='administrateur';
+            $view='connect';
+            $pagetitle='Connexion';
+            require(File::build_path(array("view","view.php")));
+        }
+    }
+
 }
-
-
