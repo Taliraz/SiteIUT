63cc582b87755c60d4dfb795f5dcd28c4faab625
Cao <cao34970@gmail.com>
2018-12-06 19:35:06 +0100

    Temoignages
    Co-Authored-By: Titouan <titouancao@users.noreply.github.com>

--- ONE-Page/Accueil.php
+++ ONE-Page/Accueil.php
@@ -1,21 +1,21 @@
+
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="Accueil.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo File::build_path_css(array("ONE-Page","Accueil.css")) ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8">
         <title>One-page</title>
-        
     </head>
     <body>
         
         <video id="video" class="media" muted autoplay loop>
-              <source src="videos/info.mp4" type="video/mp4">
+              <source src="<?php echo File::build_path_css(array("ONE-Page","videos", "info.mp4")) ?>" type="video/mp4">
         </video> 
 
         <div>
             <h1 data-scroll="toggle(.scaleDownIn, .scaleDownOut)">Bonjour!</h1>
             <p data-scroll="toggle(.scaleDownIn, .scaleDownOut)" class="texte">et Bienvenue !</p>
-            <p><image src="images/fleche.png" alt="fleche" id="fleche"></image></p>
+            <p><image src="<?php echo File::build_path_css(array("ONE-Page","images", "fleche.png")) ?>" alt="fleche" id="fleche"></image></p>
         </div>
         
         <div id="slide1Intro" class="slides">
@@ -23,151 +23,12 @@
         </div>
         
         <div id="slide1Middle" class="slides">
-            <h1 data-scroll="toggle(.scaleDownIn, .scaleDownOut)" id="titreSlide1">Trouver un IUT</h1>
-            <p data-scroll="toggle(.scaleDownIn, .scaleDownOut)" class="texte" id="texte">
-            Si sa fortune était petite,
-            Elle était sûre tout au moins.
-A la fin, les trésors déchargés sur la plage
-Le tentèrent si bien qu'il vendit son troupeau,
-Trafiqua (2) de l'argent, le mit entier(3) sur l'eau.
-            Cet argent périt par naufrage.
-Son maître fut réduit à garder les brebis,
-Non plus berger en chef comme il était jadis,
-Quand ses propres moutons paissaient sur le rivage:
-Celui qui s'était vu Coridon ou Tircis (4)
-            Fut Pierrot (4) et rien davantage.
-Au bout de quelque temps, il fit quelques profits,
-            Racheta des bêtes à laine ;
-Et comme un jour les vents retenant leur haleine
-Laissaient paisiblement aborder les vaisseaux :
-Vous voulez de l'argent, ô Mesdames les Eaux,
-Dit-il, adressez-vous, je vous prie, à quelque autre:
-            Ma foi, vous n'aurez pas le nôtre.
+            <h1 data-scroll="toggle(.scaleDownIn, .scaleDownOut)" id="titreSlide1">Ce qu'ils pensent de nous !</h1>
+    
+            <?php require_once File::build_path(array("ONE-Page","Carousel.php")); ?><br><br>
 
-Ceci n'est pas un conte à plaisir inventé.
-            Je me sers de la vérité
-            Pour montrer par expérience,
-            Qu'un sou quand il est assuré
-            Vaut mieux que cinq en espérance (5) ;
-Qu'il se faut contenter de sa condition ;
-Qu'aux conseils de la mer et de l'ambition
-            Nous devons fermer les oreilles.
-Pour un qui s'en louera, dix mille s'en plaindront.
-            La mer promet monts et merveilles :
-Fiez-vous y, les vents et les voleurs viendront.
-
-</p>
-<p data-scroll="toggle(.scaleDownIn, .scaleDownOut)" class="texte" id="texte">
-	 
-Sagesse et prudence dans la gestion de nos biens,
-c'est le conseil de La Fontaine ... 
-                Elle était sûre tout au moins.
-A la fin, les trésors déchargés sur la plage
-Le tentèrent si bien qu'il vendit son troupeau,
-Trafiqua (2) de l'argent, le mit entier(3) sur l'eau.
-            Cet argent périt par naufrage.
-Son maître fut réduit à garder les brebis,
-Non plus berger en chef comme il était jadis,
-Quand ses propres moutons paissaient sur le rivage:
-Celui qui s'était vu Coridon ou Tircis (4)
-            Fut Pierrot (4) et rien davantage.
-Au bout de quelque temps, il fit quelques profits,
-            Racheta des bêtes à laine ;
-Et comme un jour les vents retenant leur haleine
-Laissaient paisiblement aborder les vaisseaux :
-Vous voulez de l'argent, ô Mesdames les Eaux,
-Dit-il, adressez-vous, je vous prie, à quelque autre:
-            Ma foi, vous n'aurez pas le nôtre.
-
-Ceci n'est pas un conte à plaisir inventé.
-            Je me sers de la vérité
-            Pour montrer par expérience,
-            Qu'un sou quand il est assuré
-            Vaut mieux que cinq en espérance (5) ;
-Qu'il se faut contenter de sa condition ;
-Qu'aux conseils de la mer et de l'ambition
-            Nous devons fermer les oreilles.
-Pour un qui s'en louera, dix mille s'en plaindront.
-            La mer promet monts et merveilles :
-Fiez-vous y, les vents et les voleurs viendront.
-            
-</p>
-<p data-scroll="toggle(.scaleDownIn, .scaleDownOut)" class="texte" id="texte">
-		 
-Sagesse et prudence dans la gestion de nos biens,
-c'est le conseil de La Fontaine ... 
-                Elle était sûre tout au moins.
-A la fin, les trésors déchargés sur la plage
-Le tentèrent si bien qu'il vendit son troupeau,
-Trafiqua (2) de l'argent, le mit entier(3) sur l'eau.
-            Cet argent périt par naufrage.
-Son maître fut réduit à garder les brebis,
-Non plus berger en chef comme il était jadis,
-Quand ses propres moutons paissaient sur le rivage:
-Celui qui s'était vu Coridon ou Tircis (4)
-            Fut Pierrot (4) et rien davantage.
-Au bout de quelque temps, il fit quelques profits,
-            Racheta des bêtes à laine ;
-Et comme un jour les vents retenant leur haleine
-Laissaient paisiblement aborder les vaisseaux :
-Vous voulez de l'argent, ô Mesdames les Eaux,
-Dit-il, adressez-vous, je vous prie, à quelque autre:
-            Ma foi, vous n'aurez pas le nôtre.
-
-Ceci n'est pas un conte à plaisir inventé.
-            Je me sers de la vérité
-            Pour montrer par expérience,
-            Qu'un sou quand il est assuré
-            Vaut mieux que cinq en espérance (5) ;
-Qu'il se faut contenter de sa condition ;
-Qu'aux conseils de la mer et de l'ambition
-            Nous devons fermer les oreilles.
-Pour un qui s'en louera, dix mille s'en plaindront.
-            La mer promet monts et merveilles :
-Fiez-vous y, les vents et les voleurs viendront.
-            
-</p>
-<p data-scroll="toggle(.scaleDownIn, .scaleDownOut)" class="texte" id="texte">
-	
-	 
-Sagesse et prudence dans la gestion de nos biens,
-c'est le conseil de La Fontaine ... 
-                Elle était sûre tout au moins.
-A la fin, les trésors déchargés sur la plage
-Le tentèrent si bien qu'il vendit son troupeau,
-Trafiqua (2) de l'argent, le mit entier(3) sur l'eau.
-            Cet argent périt par naufrage.
-Son maître fut réduit à garder les brebis,
-Non plus berger en chef comme il était jadis,
-Quand ses propres moutons paissaient sur le rivage:
-Celui qui s'était vu Coridon ou Tircis (4)
-            Fut Pierrot (4) et rien davantage.
-Au bout de quelque temps, il fit quelques profits,
-            Racheta des bêtes à laine ;
-Et comme un jour les vents retenant leur haleine
-Laissaient paisiblement aborder les vaisseaux :
-Vous voulez de l'argent, ô Mesdames les Eaux,
-Dit-il, adressez-vous, je vous prie, à quelque autre:
-            Ma foi, vous n'aurez pas le nôtre.
-
-Ceci n'est pas un conte à plaisir inventé.
-            Je me sers de la vérité
-            Pour montrer par expérience,
-            Qu'un sou quand il est assuré
-            Vaut mieux que cinq en espérance (5) ;
-Qu'il se faut contenter de sa condition ;
-Qu'aux conseils de la mer et de l'ambition
-            Nous devons fermer les oreilles.
-Pour un qui s'en louera, dix mille s'en plaindront.
-            La mer promet monts et merveilles :
-Fiez-vous y, les vents et les voleurs viendront.
-
-	 
-Sagesse et prudence dans la gestion de nos biens,
-c'est le conseil de La Fontaine ... 
-            </p>
             <p data-scroll="toggle(.fromTopIn, .fromTopOut)">
-                <img src="images/info.png" alt="bob" id="imageSolo" class="media">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="imageSolo" class="media">
                 <span></span>
             </p>
         </div>
@@ -294,48 +155,51 @@ c'est le conseil de La Fontaine ...
             <h1 data-scroll="toggle(.scaleDownIn, .scaleDownOut)" style="text-shadow:4px 4px black;margin-top:100px;"></h1>
         </div>
         <div id="slide3Back" class="slides">
-            <image src='images/gif.gif' alt="image" class="media"></image>
+            <image src='<?php echo File::build_path_css(array("ONE-Page","images", "gif.gif")) ?>' alt="image" class="media"></image>
         </div>
         <div id="slide3End" class="slides">
         </div>
         <div id="slide3Sup" class="slides">
             <p></p> <!-- NE PAS SUPPRIMER -->
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image1" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image1" class="scene-pic" class="scene-pic">
             </p>
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image2" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image2" class="scene-pic" class="scene-pic">
             </p>
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image3" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image3" class="scene-pic" class="scene-pic">
             </p>
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image4" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image4" class="scene-pic" class="scene-pic">
             </p>
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image5" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image5" class="scene-pic" class="scene-pic">
             </p>
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image6" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image6" class="scene-pic" class="scene-pic">
             </p>
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image7" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image7" class="scene-pic" class="scene-pic">
             </p>
             <p data-depth="0.3" class="scene">
-                <img src="images/info.png" alt="bob" id="image8" class="scene-pic" class="scene-pic">
+                <img src="<?php echo File::build_path_css(array("ONE-Page","images", "info.png")) ?>" alt="bob" id="image8" class="scene-pic" class="scene-pic">
             </p>
        </div>
         <div id="slide4" class="slides">
-            <image src="images/carte.png" alt="image" class="media"></image>
+            <image src="<?php echo File::build_path_css(array("ONE-Page","images", "carte.png")) ?>" alt="image" class="media"></image>
         </div>
         
         
         
         
-        <script src="parallax/parallax.js"></script>
-        <script src="ScrollTrigger/ScrollTrigger.min.js"></script>
-        <script src="Trigger.js"></script>
-        <script src="Parallax-scroll.js"></script>
-        <script src="Parallax-souris.js"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","parallax", "parralax.js")) ?>"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","ScrollTrigger", "ScrollTrigger.min.js")) ?>"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","Trigger.js")) ?>"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","Parallax-scroll.js")) ?>"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","Parallax-souris.js")) ?>"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","JQuery.js")) ?>"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","JCarousel.js")) ?>"></script>
+        <script type="text/javascript" src="<?php echo File::build_path_css(array("ONE-Page","jcarousel.transitions.js")) ?>"></script>
     </body>
 </html>
---
+++ ONE-Page/JCarousel.js
@@ -0,0 +1,67 @@
+// Execution de cette fonction lorsque le DOM sera entièrement chargé
+$(document).ready(function() {
+     var fleche_l = document.getElementsByClassName("fleche_l")[0]
+     fleche_l.style.display = "none" 
+	// Calcul préalables :
+		// Element de référence pour la zone de visualisation (ici le premier item)
+		Reference = $(".carousel li:first-child");
+		// Nombre d'éléments de la liste
+		NbElement = $(".carousel .tem_tit").length;
+	// Ciblage de la bande de diapositives
+	$(".carousel")
+		// Englobage de la liste par la zone de visualisation
+		.wrap('<div class="carousel-conteneur"></div>')
+		// Application d'une largeur à la bande de diapositive afin de conserver une structrure horizontale
+		.css("width", (Reference.width() * NbElement) );
+	// Ciblage de la zone de visualisation
+	$(".carousel-conteneur")
+		// Application de la largeur d'une seule diapositive
+		.width(  Reference.width()  )
+		// Application de la hauteur d'une seule diapositive
+		.height( Reference.height() )
+		// Blocage des débordements
+		.css("overflow", "hidden");
+    $(".carousel-conteneur")
+		// Application de la largeur d'une seule diapositive
+		.width(  Reference.width()  )
+		// Application de la hauteur d'une seule diapositive
+		.height( Reference.height() )
+		// Blocage des débordements
+		.css("overflow", "hidden")
+		// Insertion des boutons de navigation
+    // Actions de navigation
+		// Initialisation du compteur
+		Cpt = 0;
+		// Clic sur le bouton "Suivant"
+
+		$(".carousel-next").click(function() {
+			// Si le compteur est inférieur au nombre de diaposives moins 1
+			if(Cpt < (NbElement-1) ) {
+				// Ajout +1 au compteur (nous allons sur la diapositive suivante)
+				Cpt++;
+				// Mouvement du carrousel en arrière-plan
+				$(".carousel").animate({
+					marginLeft : - (Reference.width() * Cpt)
+				});
+			} 
+            if (Cpt == NbElement-1) {
+                var fleche_d = document.getElementsByClassName("fleche_d")[Cpt]
+                fleche_d.style.display = "none"
+            }
+            // fin du if
+		});
+		// Action du bouton "Précédent"
+		$(".carousel-prev").click(function() {
+			// Si le compteur est supérieur à zéro
+			if(Cpt > 0) {
+
+				// Soustraction -1 au compteur (nous allons sur la diapositive précédente)
+				Cpt--;
+				// Mouvement du carrousel en arrière-plan
+				$(".carousel").animate({
+					marginLeft : - (Reference.width() * Cpt)
+				});
+			} 
+            // fin du if
+		});
+});
