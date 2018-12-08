9a076258f23eceafc90c1d29e5d454156ceb0ff6
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2018-12-08 13:21:19 +0100

    Update PanelAdmin.php

--- view/PanelAdmin.php
+++ view/PanelAdmin.php
@@ -1,22 +1,38 @@
-
+<!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8" />
+        <meta charset="UTF-8">
         <link rel="stylesheet" type="text/css" href="<?php echo File::build_path_css(array("view","CSS" ,"PanelAdmin.css")); ?>">
-        <title> Interface Administrateur </title>
+        <title><?php echo $pagetitle; ?></title>
     </head>
+
+     <header>
+        
+            
+            <?php
+            if(isset($_SESSION['login'])){
+                echo '<ul id="colonne"><a href="#"><li id="titre"></li></a>
+                    <a href="admin.php?action=disconnect&controller=administrateur&login="'.$_SESSION['login'].'><li class="element-liste"> deconnexion </li></a>
+                    <a  href="admin.php?action=readAll&controller=administrateur"><li class="element-liste"> Administrateur</li> </a>
+                    <a  href="admin.php?action=readAll&controller=article"><li class="element-liste"> Articles</li> </a>
+                    <a  href="admin.php?action=readAll&controller=IUT">IUT<li class="element-liste"></li> </a>
+                    <a  href="admin.php?action=readAll&controller=stage"><li class="element-liste">Stage </li></a>
+                    <a  href="admin.php?action=readAll&controller=temoignage"><li class="element-liste">Temoignage </li></a>
+                    <a  href="admin.php?action=readAll&controller=ville"><li class="element-liste">Ville</li> </a>
+                    </ul>';
+            }
+            
+            ?>
+
+    </header>
+
     <body>
-        <ul id="colonne">
-            <a href="#"><li id="titre"></li></a>
-            <a href="http://localhost/Admin/SiteIUT/Admin.php"><li class="element-liste">Etudiants</li></a>
-            <a href="#"><li class="element-liste">LIGNE 2</li></a>
-            <a href="#"><li class="element-liste">LIGNE 3</li></a>
-            <a href="#"><li class="element-liste">LIGNE 4</li></a>
-            <a href="#"><li class="element-liste">LIGNE 5</li></a>
-            <a href="#"><li class="element-liste">LIGNE 6</li></a>
-            <a href="#"><li class="element-liste">LIGNE 7</li></a>
-            <a href="#"><li class="element-liste">LIGNE 8</li></a>
-            <a href="#"><li class="element-liste">LIGNE 9</li></a>
-        </ul>
+
+    <?php
+    $filepath = File::build_path(array("view", $controller, $view.".php"));
+    require $filepath;
+    ?>
     </body>
-</html> 
+
+
+</html>
