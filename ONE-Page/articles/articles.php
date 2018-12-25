723312697ee3d204121090ab3cf96b27424a0d7b
Cao <cao34970@gmail.com>
2018-12-25 22:15:40 +0100

    Style
    Co-Authored-By: Titouan <titouancao@users.noreply.github.com>

--- ONE-Page/articles/articles.php
+++ ONE-Page/articles/articles.php
@@ -1,4 +1,3 @@
-   
 
 <ul class="liste_articles">
     <?php 
@@ -6,22 +5,55 @@
     $art = ModelArticle::getAllArticles();
     $req = Model::$pdo->query('SELECT COUNT(idArticle) FROM `mon-Articles`');
     $total_articles = $req->fetch();
+    $total = intval($total_articles[0]);
     $cpt = 0;
-    foreach($art as $key){ 
-        $cpt++;
-        echo '
-            <li class="article">
-                <p class="nom_article">'.$key->getNomArticle().'</p>
-                <p class="contenu_article">'.$key->getContenuArticle() .'</p>
-            ';
+    
         
-        if ($cpt < intval($total_articles[0])){
-            echo '<br><div class="ligne_article"></div>';
+    echo '
+    <select id="selectArt" onchange="artGoTo()">';
+        foreach($art as $key) {
+            echo '<option value="'.$key->getIdArticle().'">'.$key->getNomArticle().'</option>';
         }
+        
+    echo '</select> '; 
+    
+    foreach($art as $key){ 
+        $id = $key->getIdArticle();
+        
+        
+        if ($cpt < $total){
+            $reqNext = Model::$pdo->query("SELECT nomArticle, idArticle FROM `mon-Articles` WHERE idArticle = (select min(idArticle) from `mon-Articles` where idArticle > $id)");
+            $reqPrev = Model::$pdo->query("SELECT nomArticle, idArticle FROM `mon-Articles` WHERE idArticle = (select max(idArticle) from `mon-Articles` where idArticle < $id)");
+            $nextNom = $reqNext->fetch();
+            $prevNom = $reqPrev->fetch();
+        }
+        
+        $cpt++;
+        
+        
+        echo '<li class="article">
+                <h1 class="nomArticle">'.$key->getNomArticle().'</h1>
+                <span id="'.$id.'" class="getArtId"></span>
+                <div class="artHeader">';
                 
-        echo '
-            </li>
-            ';
-    }
+        if ($prevNom != NULL){
+            echo '<p class="InfosPrev">Précédent : <br>'.$prevNom[0].'</p>';
+        }
+        else {
+            echo '<p class="InfosPrev"></p>';
+        }
+        
+        if($nextNom != NULL){
+            echo '<p class="InfosNext">Suivant : <br>'.$nextNom[0].'</p>';
+        }
+        else {
+            echo '<p class="InfosNext"></p>';
+        }
+        
+        echo '</div>
+                <p class="contenu_article">'.$key->getContenuArticle() .'</p>
+            </li>';
+        }
     ?>
 </ul>
+<div id="switcher"></div>
--- ONE-Page/parallax/Parallax-scroll.js
+++ ONE-Page/parallax/Parallax-scroll.js
@@ -5,22 +5,19 @@ window.requestAnimationFrame = window.requestAnimationFrame
                                || window.msRequestAnimationFrame
                                || function(f){setTimeout(f, 1000/60)}
 
-var video = document.getElementById('video')
-var slide1Intro = document.getElementById('slide1Intro')
-var slide1Middle = document.getElementById('slide1Middle')
-var slide1End = document.getElementById('slide1End')
+var slide0 = document.getElementById('slide0')
+var slide1 = document.getElementById('slide1')
 var slide2 = document.getElementById('slide2')
-var slide3Begin = document.getElementById('slide3Begin')
 var slide3 = document.getElementById('slide3')
 var slide4 = document.getElementById('slide4')
 
-
+var v2 = 0.7
 
 var classArticles = slide3.getElementsByClassName("liste_articles")
 var nb_articles = classArticles.length;
 var hauteur_articles = Math.ceil(nb_articles/2);
 
-
+var pageEnd = slide0.clientHeight + slide1.clientHeight + slide2.clientHeight + slide3.clientHeight + slide4.clientHeight 
 
 function viewportToPixels(value) {
   var parts = value.match(/([0-9\.]+)(vh|vw)/)
@@ -30,26 +27,12 @@ function viewportToPixels(value) {
 }
 
 
-window.onload = function() {  
-    if(window.innerWidth>600){
-        slide3.style.height = hauteur_articles*(classArticles[0].clientHeight+viewportToPixels('10vh')) + "px";
-    }
-    else {
-        slide3.style.height = nb_articles*(classArticles[0].clientHeight+viewportToPixels('5vh')) + "px";
-    }    
-    
-    document.body.style.height = video.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight - 100 + slide4.clientHeight + slide2.clientHeight + "px";
+window.onload = function() {   
+    document.body.style.height = pageEnd + "px";
 }
 
-window.onresize = function() {
-    if(window.innerWidth>600){
-        slide3.style.height = hauteur_articles*(classArticles[0].clientHeight+viewportToPixels('10vh')) + "px";
-    }
-    else {
-        slide3.style.height = nb_articles*(classArticles[0].clientHeight+viewportToPixels('5vh')) + "px";
-    }
-    
-    document.body.style.height = video.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight - 100 + slide4.clientHeight + slide2.clientHeight + "px";
+window.onresize = function() {  
+    document.body.style.height = pageEnd + "px";
 }
 
 
@@ -67,75 +50,42 @@ function parallaxScroll(objet, vitesse, yOrigin){
 
 
 
-//Video
 
-window.addEventListener('load', function(){ 
-	requestAnimationFrame(parallaxScroll(video, 0.1, 0))
-}, false)
-
-window.addEventListener('scroll', function(){ 
-	requestAnimationFrame(parallaxScroll(video, 0.1, 0)) 
-}, false)
-
-
-
-
-//Premier Slide - Première Partie
+//Slide 0 - Haut de page
 
 window.addEventListener('load', function(){
-     slide1Intro.style.height = video.clientHeight + "px";
-	requestAnimationFrame(parallaxScroll(slide1Intro, 1, 0)) 
+     slide0.style.height = slide0.clientHeight + "px";
+	requestAnimationFrame(parallaxScroll(slide0, 1, 0)) 
 }, false)
 
 window.addEventListener('resize', function(){ 
-     slide1Intro.style.height = video.clientHeight + "px";
-	requestAnimationFrame(parallaxScroll(slide1Intro, 1, 0)) 
+     slide0.style.height = slide0.clientHeight + "px";
+	requestAnimationFrame(parallaxScroll(slide0, 1, 0)) 
 }, false)
 
 window.addEventListener('scroll', function(){ 
-    slide1Intro.style.height = video.clientHeight + "px";
-	requestAnimationFrame(parallaxScroll(slide1Intro, 1, 0)) 
+    slide0.style.height = slide0.clientHeight + "px";
+	requestAnimationFrame(parallaxScroll(slide0, 1, 0)) 
 }, false)
 
 
 
 
-//Premier Slide - Deuxième Partie
+//Premier Slide
 
 window.addEventListener('load', function(){ 
-	requestAnimationFrame(parallaxScroll(slide1Middle, 1, slide1Intro.clientHeight)) 
+	requestAnimationFrame(parallaxScroll(slide1, 1, slide0.clientHeight)) 
 }, false)
 
 window.addEventListener('resize', function(){ 
-	requestAnimationFrame(parallaxScroll(slide1Middle, 1, slide1Intro.clientHeight)) 
+	requestAnimationFrame(parallaxScroll(slide1, 1, slide0.clientHeight)) 
 }, false)
 
 window.addEventListener('scroll', function(){ 
-	requestAnimationFrame(parallaxScroll(slide1Middle, 1, slide1Intro.clientHeight))
+	requestAnimationFrame(parallaxScroll(slide1, 1, slide0.clientHeight))
 }, false)
 
 
-
-
-
-
-
-//Premier Slide - Troisième Partie
-
-window.addEventListener('load', function(){ 
-    var h1 = slide1Intro.clientHeight;
-	requestAnimationFrame(parallaxScroll(slide1End, 1, h1+slide1Middle.clientHeight - 1))
-}, false)
-
-window.addEventListener('resize', function(){ 
-    var h1 = slide1Intro.clientHeight;
-	requestAnimationFrame(parallaxScroll(slide1End, 1, h1+slide1Middle.clientHeight - 1)) 
-}, false)
-
-window.addEventListener('scroll', function(){ 
-    var h1 = slide1Intro.clientHeight;
-	requestAnimationFrame(parallaxScroll(slide1End, 1, h1+slide1Middle.clientHeight - 1)) 
-}, false)
 
 
 
@@ -144,17 +94,17 @@ window.addEventListener('scroll', function(){
 var v = 0.5
 
 window.addEventListener('load', function(){ 
-    var h2 = (slide1Intro.clientHeight + slide1Middle.clientHeight)*v
+    var h2 = (slide0.clientHeight + slide1.clientHeight)*v
 	requestAnimationFrame(parallaxScroll(slide2, v, h2)) 
 }, false)
 
 window.addEventListener('resize', function(){ 
-    var h2 = (slide1Intro.clientHeight + slide1Middle.clientHeight)*v
+    var h2 = (slide0.clientHeight + slide1.clientHeight)*v
 	requestAnimationFrame(parallaxScroll(slide2, v, h2)) 
 }, false)
 
 window.addEventListener('scroll', function(){ 
-    var h2 = (slide1Intro.clientHeight + slide1Middle.clientHeight)*v
+    var h2 = (slide0.clientHeight + slide1.clientHeight)*v
 	requestAnimationFrame(parallaxScroll(slide2, v, h2)) 
 }, false)
 
@@ -162,104 +112,74 @@ window.addEventListener('scroll', function(){
 
 
 
-//Troisième Slide - Première Partie
+
+//Troisème Slide 
 
 window.addEventListener('load', function(){ 
     if(window.innerWidth>600){
-        var h3 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight - 100
+        var h4 = slide0.clientHeight + slide1.clientHeight + slide2.clientHeight
     }
     else {
-        var h3 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + 200
-    }
-	requestAnimationFrame(parallaxScroll(slide3Begin, 1, h3)) 
-}, false)
-
-window.addEventListener('resize', function(){
-    if(window.innerWidth>600){
-        var h3 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight - 100
-    }
-    else {
-        var h3 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + 200
-    }
-	requestAnimationFrame(parallaxScroll(slide3Begin, 1, h3)) 
-}, false)
-
-window.addEventListener('scroll', function(){ 
-    if(window.innerWidth>600){
-        var h3 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight - 100
-    }
-    else {
-        var h3 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + 200
-    }
-	requestAnimationFrame(parallaxScroll(slide3Begin, 1, h3))  
-}, false)
-
-
-
-//Troisème Slide - Deuxième Partie - Deuxième Couche
-
-window.addEventListener('load', function(){ 
-    if(window.innerWidth>600){
-        var h4 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + slide3Begin.clientHeight - 100
-    }
-    else {
-        var h4 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + slide3Begin.clientHeight + 200
+        var h4 = slide0.clientHeight + slide1.clientHeight + slide2.clientHeight
     }
 	requestAnimationFrame(parallaxScroll(slide3, 1, h4)) 
 }, false)
 
 window.addEventListener('resize', function(){ 
     if(window.innerWidth>600){
-        var h4 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + slide3Begin.clientHeight - 100
+        var h4 = slide0.clientHeight + slide1.clientHeight + slide2.clientHeight
     }
     else {
-        var h4 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + slide3Begin.clientHeight + 200
+        var h4 = slide0.clientHeight + slide1.clientHeight + slide2.clientHeight
     }
 	requestAnimationFrame(parallaxScroll(slide3, 1, h4)) 
 }, false)
 
 window.addEventListener('scroll', function(){ 
     if(window.innerWidth>600){
-        var h4 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + slide3Begin.clientHeight - 100
+        var h4 = slide0.clientHeight + slide1.clientHeight + slide2.clientHeight
     }
     else {
-        var h4 = slide1Intro.clientHeight + slide1Middle.clientHeight + slide2.clientHeight + slide3Begin.clientHeight + 200
+        var h4 = slide0.clientHeight + slide1.clientHeight + slide2.clientHeight
     }
 	requestAnimationFrame(parallaxScroll(slide3, 1, h4)) 
 }, false)
+
+
+
 
 
 
 //Quatrième Slide
 
-var v2 = 0.7
+
 
 window.addEventListener('load', function(){ 
     if(window.innerWidth>600){
-        var h5 = (slide1Intro.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight + slide2.clientHeight - 100)*v2
+        var h5 = (slide0.clientHeight + slide1.clientHeight +  slide3.clientHeight + slide2.clientHeight)*v2
     }
     else {
-        var h5 = (slide1Intro.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight + slide2.clientHeight + 200)*v2
+        var h5 = (slide0.clientHeight + slide1.clientHeight + slide3.clientHeight + slide2.clientHeight)*v2
     }
 	requestAnimationFrame(parallaxScroll(slide4, v2, h5)) 
 }, false)
 
 window.addEventListener('resize', function(){ 
     if(window.innerWidth>600){
-        var h5 = (slide1Intro.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight + slide2.clientHeight - 100)*v2
+        var h5 = (slide0.clientHeight + slide1.clientHeight + slide3.clientHeight + slide2.clientHeight)*v2
     }
     else {
-        var h5 = (slide1Intro.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight + slide2.clientHeight + 200)*v2
+        var h5 = (slide0.clientHeight + slide1.clientHeight +  slide3.clientHeight + slide2.clientHeight)*v2
     }
 	requestAnimationFrame(parallaxScroll(slide4, v2, h5)) 
 }, false)
 
 window.addEventListener('scroll', function(){ 
     if(window.innerWidth>600){
-        var h5 = (slide1Intro.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight + slide2.clientHeight - 100)*v2
+        var h5 = (slide0.clientHeight + slide1.clientHeight  + slide3.clientHeight + slide2.clientHeight)*v2
     }
     else {
-        var h5 = (slide1Intro.clientHeight + slide1Middle.clientHeight + slide3Begin.clientHeight + slide3.clientHeight + slide2.clientHeight + 200)*v2
+        var h5 = (slide0.clientHeight + slide1.clientHeight + slide3.clientHeight + slide2.clientHeight)*v2
     }
 	requestAnimationFrame(parallaxScroll(slide4, v2, h5)) 
 }, false)
