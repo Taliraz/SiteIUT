6cc89dd2fe91a2840517e3c0e4320e5bb26dc534
Haldrius <Jules.nofal@gmail.com>
2019-01-11 19:38:01 +0100

    Bug suppression des stages réglé

--- view/stage/list.php
+++ view/stage/list.php
@@ -5,7 +5,7 @@
         foreach ($tab_v as $v){
             echo '<p class="listeElements"> STAGE : 
                     <a class="listeElement1" title="Stage" href="admin.php?action=read&controller=stage&idStage='.rawurlencode($v->getIdStage()).'">'.htmlspecialchars($v->getIntituleStage()).'</a> 
-                    <a class="listeElementSupprimer" title="supprimer" href="admin.php?action=delete&controller=stage&id='.rawurlencode($v->getIdStage()).'">SUPPRIMER</a>;
+                    <a class="listeElementSupprimer" title="supprimer" href="admin.php?action=delete&controller=stage&idStage='.rawurlencode($v->getIdStage()).'">SUPPRIMER</a>;
                     
                 </p>';
             }  
