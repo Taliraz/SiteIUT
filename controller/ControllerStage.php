69213f8c1e48cbb9bc262eafa72a10cf741bf094
Sullyvan Armangau <43641531+Taliraz@users.noreply.github.com>
2019-01-13 15:53:50 +0100

    Update ControllerStage.php

--- controller/ControllerStage.php
+++ controller/ControllerStage.php
@@ -74,7 +74,7 @@ class ControllerStage{
             $IUT=ModelIUT::getAllIUTs();
             foreach ($IUT as $value) {
                 if($value->getMailSecretariatIUT()!=NULL){
-                    mail($value->getMailSecretariatIUT(),"Nouveau Stage ".$v->getIntituleStage(),$mail);
+                    mail($value->getMailSecretariatIUT(),"Nouveau Stage de ".$v->getIntituleStage()." chez ".$v->getNomEntreprise(),$mail);
                 }            }
             $controller='stage';
             $view='created';
